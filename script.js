61f48033e486f614e72c708a6ad67cec038630c1
katrinawoods <75281195+katrinawoods@users.noreply.github.com>
2023-09-10 11:15:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -87,29 +87,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 
-    function checkAnswer() {
-        let cards = document.querySelectorAll('.card');
-        let isCorrect = true;
+  function checkAnswer() {
+    let cards = document.querySelectorAll('.card');
+    let isCorrect = true;
 
-        cards.forEach((card, index) => {
-            card.classList.remove('green', 'red');  // Reset previous feedback
+    cards.forEach((card, index) => {
+        card.classList.remove('green', 'red');  // Reset previous feedback
 
-            if (card.innerHTML.trim() !== correctOrder[index]) {
-                card.classList.add('red');
-                console.log(`Card at index ${index} is incorrect. Expected: ${correctOrder[index]}, Found: ${card.innerHTML.trim()}`);
-                isCorrect = false;
-            } else {
-                card.classList.add('green');
-                console.log(`Card at index ${index} is correct.`);
-            }
-        });
+        // Remove existing icons (if any)
+        let existingTickIcon = card.querySelector('.icon.tick');
+        let existingCrossIcon = card.querySelector('.icon.cross');
 
-        const feedbackEl = document.getElementById('feedback');
-        if (isCorrect) {
-            feedbackEl.textContent = "Correct! Well done.";
+        if (existingTickIcon) existingTickIcon.remove();
+        if (existingCrossIcon) existingCrossIcon.remove();
+
+        // Add the tick and cross icons
+        let tickIcon = document.createElement('span');
+        tickIcon.classList.add('icon', 'tick');
+        tickIcon.innerHTML = '✓';
+        card.appendChild(tickIcon);
+
+        let crossIcon = document.createElement('span');
+        crossIcon.classList.add('icon', 'cross');
+        crossIcon.innerHTML = '✗';
+        card.appendChild(crossIcon);
+
+        if (card.innerHTML.trim() !== correctOrder[index]) {
+            card.classList.add('red');
+            console.log(`Card at index ${index} is incorrect. Expected: ${correctOrder[index]}, Found: ${card.innerHTML.trim()}`);
+            isCorrect = false;
         } else {
-            feedbackEl.textContent = "Incorrect. Please try again.";
+            card.classList.add('green');
+            console.log(`Card at index ${index} is correct.`);
         }
-    }
+    });
 
-});
+    const feedbackEl = document.getElementById('feedback');
+    if (isCorrect) {
+        feedbackEl.textContent = "Correct! Well done.";
+    } else {
+        feedbackEl.textContent = "Incorrect. Please try again.";
+    }
+}
+
